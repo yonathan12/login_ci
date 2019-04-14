64b07e33b2467dd5ac11802b4985efbf9af4fad8
yonathan rizky <jrizky.jonathan@gmail.com>
2019-04-14 22:48:54 +0700

    Day 6

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -11,35 +11,50 @@
 
 <!-- Divider -->
 <hr class="sidebar-divider">
+<!-- Query Menu -->
+<?php
+$role_id = $this->session->userdata('role_id');
+  $queryMenu = "SELECT user_menu.Id, user_menu.menu
+                FROM user_menu JOIN user_access_menu
+                ON user_menu.Id = user_access_menu.menu_id
+                WHERE user_access_menu.role_id = $role_id
+                ORDER BY user_access_menu.menu_id ASC
+                ";
+  $menu = $this->db->query($queryMenu)->result_array();
+?>
+<!-- End Query Menu -->
 
+<!-- LOOPING MENU -->
 <!-- Heading -->
+<?php foreach ($menu as $m) : ?>
 <div class="sidebar-heading">
-  Administrator
+  <?= $m['menu']; ?>
 </div>
-
+<!-- Siapkan Sub Menu -->
+<?php
+$menuId = $m['Id'];
+$querySubMenu = "SELECT *
+                FROM user_sub_menu JOIN user_menu
+                ON user_sub_menu.menu_id = user_menu.Id
+                WHERE user_sub_menu.menu_id = $menuId
+                AND user_sub_menu.is_active = 1
+";
+$subMenu = $this->db->query($querySubMenu)->result_array();
+?>
+<?php foreach($subMenu as $sm) :?>
 <!-- Nav Item - Dashboard -->
 <li class="nav-item">
-  <a class="nav-link" href="index.html">
-    <i class="fas fa-fw fa-tachometer-alt"></i>
-    <span>Dashboard</span></a>
+  <a class="nav-link" href="<?= base_url($sm['url']); ?>">
+    <i class="<?= $sm['icon']; ?>"></i>
+    <span><?= $sm['title']; ?></span></a>
 </li>
+
+<?php endforeach; ?>
 
 <!-- Divider -->
 <hr class="sidebar-divider">
-
-<!-- Heading -->
-<div class="sidebar-heading">
-  User
-</div>
- <!-- Nav Item - Charts -->
-<li class="nav-item">
-  <a class="nav-link" href="charts.html">
-    <i class="fas fa-fw fa-fw fa-user"></i>
-    <span>My Profile</span></a>
-</li>
-
-<!-- Divider -->
-<hr class="sidebar-divider">
+<!-- End sub menu -->
+<?php endforeach; ?>
 
  <!-- Nav Item - Charts -->
 <li class="nav-item">
